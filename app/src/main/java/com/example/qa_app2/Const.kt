b2765d5f070adb0586220775431b23c2e5ba54aa
sasaki <aaa@aaa>
2019-05-26 08:12:50 +0900

    Initial commit

---
+++ app/src/main/java/com/example/qa_app2/Const.kt
@@ -0,0 +1,7 @@
+package com.example.qa_app2
+
+    const val UsersPATH = "users"
+    const val ContentsPATH = "contents"
+    const val AnswersPATH = "answers"
+    const val FavoritePATH = "favorite"
+    const val NameKEY = "name"
